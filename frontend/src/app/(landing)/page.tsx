6774394f179be79ddf603ff64546f3206e0748ed
imdigitalashish <imdigitalashish@gmail.com>
2025-03-23 19:46:47 +0530

    Forntend

---
+++ frontend/src/app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LandingPageHeroComponent from './_components/LandingPageHeroComponent'
+
+export default function page() {
+  return (
+    <div>
+        <LandingPageHeroComponent />
+    </div>
+  )
+}
